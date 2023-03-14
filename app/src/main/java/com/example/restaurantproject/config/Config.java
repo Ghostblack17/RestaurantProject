64855d3b9c04f1d8b8e7694c7ddcaca00fa1cf36
Black_Ghost17 <ghostwing17@gmail.com>
2023-03-14 15:09:44 +0900

    레스토랑 앱

---
+++ app/src/main/java/com/example/restaurantproject/config/Config.java
@@ -0,0 +1,14 @@
+package com.example.restaurantproject.config;
+
+public class Config {
+
+    public static final String BASE_URL = "https://tsnctg9o31.execute-api.ap-northeast-2.amazonaws.com";
+//    public static final String TEST_URL = "http://localhost:5000";
+    public static final String PREFERENCES_NAME = "RestaurantProject";
+
+    public static final String IMAGE_URL = "https://matchrun-image.s3.ap-northeast-2.amazonaws.com/";
+
+    public static final String GOOGLE_MAPS_BASE_URL = "https://tsnctg9o31.execute-api.ap-northeast-2.amazonaws.com";
+
+
+}
